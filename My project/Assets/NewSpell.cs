34ba34ec1b8acc7fa25afd10986c7f2acc981240
GinnieMion <virginiamay.delacruz.120@accesscreative.ac.uk>
2026-01-13 15:17:16 +0000

    Added panel accessibility

--- My project/Assets/NewSpell.cs
+++ My project/Assets/NewSpell.cs
@@ -1,10 +1,40 @@
 using UnityEngine;
+using TMPro;
 
 public class NewSpell : MonoBehaviour
+
 {
     string spellone = "Fireballuh";
     string spelltwo = "Wateruh";
     string spellthree = "Winduh";
     string spellfour = "Rockuh";
 
+    public GameObject MenuPanel;
+    bool opentosee = false;
+
+    // i didnt use openpanel here since I didn't want the variables to end up clashing the code later on.
+
+    void Start()
+    {
+        MenuPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+    public void slotOne()
+    {
+        Debug.Log("Click recognised.");
+        if (opentosee == false)
+        {
+            MenuPanel.SetActive(true);
+            opentosee = true;
+        }
+        else
+        {
+            MenuPanel.SetActive(false);
+            opentosee = false;
+        }
+    }
 }
