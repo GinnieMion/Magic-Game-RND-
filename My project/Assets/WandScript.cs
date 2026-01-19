00e54b30eac322c6d9195b3b6b4e0483384e7d69
GinnieMion <virginiamay.delacruz.120@accesscreative.ac.uk>
2026-01-19 12:19:42 +0000

    Fireball revamp + Delay

--- My project/Assets/WandScript.cs
+++ My project/Assets/WandScript.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class WandScript : MonoBehaviour
@@ -5,6 +6,8 @@ public class WandScript : MonoBehaviour
     public Transform SpellSpawnPoint;
     public GameObject SpellPrefab;
     public float spellSpeed = 100;
+    public bool cooldown;
+    public float delayTime = 0.5f;
 
     private void Start()
     {
@@ -15,11 +18,19 @@ public class WandScript : MonoBehaviour
 
         if (enabled)
         {
-            if (Input.GetKeyUp(KeyCode.Mouse0))
+            if (Input.GetKeyUp(KeyCode.Mouse0) && !cooldown)
             {
+                cooldown = true;
                 var spell = Instantiate(SpellPrefab, SpellSpawnPoint.position, SpellSpawnPoint.rotation);
                 spell.GetComponent<Rigidbody>().linearVelocity = SpellSpawnPoint.forward * spellSpeed;
+                StartCoroutine(timer());
             }
         }
+
+    }
+    public IEnumerator timer()
+    {
+        yield return new WaitForSeconds(delayTime);
+        cooldown = false;
     }
 }
